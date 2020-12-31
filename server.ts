827473fc710e69cc6804840771e5a3a4b2b3870d
chkim116 <dudnqnfqlc@naver.com>
2021-01-01 01:29:05 +0900

    Feat : 프로덕션시 origin url 추가

--- server.ts
+++ server.ts
@@ -29,7 +29,7 @@ app.use(
     cors({
         origin:
             process.env.NODE_ENV === "production"
-                ? "https://jaswiki.com"
+                ? ["https://jaswiki.com", "https://jaswiki.netlify.app"]
                 : "http://localhost:3000",
         credentials: true,
     })
